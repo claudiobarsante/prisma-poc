992bf965c2c7ec07f9352999cdc26156a13347f8
Claudio Barsante <clbmribas@gmail.com>
2021-10-21 17:39:33 -0300

    feat: add graphql endpoint and configuration for Apollo studio to work

---
+++ pages/api/graphql.ts
@@ -0,0 +1,33 @@
+import { ApolloServer } from 'apollo-server-micro';
+import { typeDefs } from '../../graphql/schema';
+import { resolvers } from '../../graphql/resolvers';
+import Cors from 'micro-cors';
+
+const cors = Cors();
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+const startServer = apolloServer.start();
+
+export default cors(async function handler(req, res) {
+  // -- for Apollo studio to work
+  if (req.method === 'OPTIONS') {
+    res.end();
+    return false;
+  }
+  await startServer;
+
+  await apolloServer.createHandler({
+    path: '/api/graphql'
+  })(req, res);
+});
+
+// -- https://nextjs.org/docs/api-routes/api-middlewares
+// -- bodyParser Enables body parsing,
+// -- the default config on Next it's json
+// -- to use GraphQL playground
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
