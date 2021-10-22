4ab6d9d7d0c656541fe0feb290005c18f015ebec
Claudio Barsante <clbmribas@gmail.com>
2021-10-22 17:25:49 -0300

    feat: set up Apollo client and add ALL_LINKS_QUERY to index.tsx

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,6 +1,7 @@
 // graphql/schema.ts
-
-import { gql } from 'apollo-server-micro';
+/**
+ * First aproach defining schemas manually
+ * import { gql } from 'apollo-server-micro';
 
 export const typeDefs = gql`
   type Link {
@@ -17,3 +18,31 @@ export const typeDefs = gql`
     links: [Link]!
   }
 `;
+ */
+
+// /graphql/schema.ts
+import { makeSchema } from 'nexus';
+import { join } from 'path';
+import * as types from './types';
+
+// -- schema will be generated in the folder graphql with name schema.graphql
+// -- will use the context the we already created in folder graphql
+// -- Make sure the server is running and navigate to http://localhost:3000/api/graphql
+// -- to generate the schema and everytime you need to regenerate it
+export const schema = makeSchema({
+  types,
+  outputs: {
+    typegen: join(
+      process.cwd(),
+      'node_modules',
+      '@types',
+      'nexus-typegen',
+      'index.d.ts'
+    ),
+    schema: join(process.cwd(), 'graphql', 'schema.graphql')
+  },
+  contextType: {
+    export: 'Context',
+    module: join(process.cwd(), 'graphql', 'context.ts')
+  }
+});
